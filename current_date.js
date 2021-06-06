3bb29947c9a179c2af13ada54cae5d7fa65438d6
JavierSosa2021 <sos17001@byui.edu>
2021-06-06 18:38:48 -0400

    update

--- current_date.js
+++ current_date.js
@@ -2,4 +2,4 @@ const lastModified = document.lastModified;
 document.querySelector("#Date_Modified").textContent = lastModified;
 
 const year = new Date().getFullYear();
-document.querySelector("#Current_Year").textContent = year;
+ document.querySelector("#Current_Year").textContent = year;
